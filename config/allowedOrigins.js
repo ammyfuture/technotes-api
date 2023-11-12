dd9180bb908f42d73010b94b7da18d7200505126
Amy Saeed <ammythecreator@gmail.com>
2023-11-12 07:28:06 -0800

    fixing the allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   // my front end is the only one allowed to send req to my backend
   "https://technotes.onrender.com",
+  "https://technotes-m7ma.onrender.com",
 ];
 
 module.exports = allowedOrigins;
